31b3b59a289aef406ecdb3dd41658aa40146a0ce
Andrew K <andrewjk@users.noreply.github.com>
2026-02-19 14:40:42 +1100

    Test: add more tests

---
+++ dotnet/Allmark.Tests/HeadingTests.cs
@@ -0,0 +1,394 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Allmark.Rulesets;
+
+namespace Allmark.Tests;
+
+[TestClass]
+public class HeadingTests
+{
+	[TestMethod]
+	public void AtxHeadingLevel1()
+	{
+		var input = "# Heading 1";
+		var expected = """
+		<h1>Heading 1</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingLevel2()
+	{
+		var input = "## Heading 2";
+		var expected = """
+		<h2>Heading 2</h2>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingLevel3()
+	{
+		var input = "### Heading 3";
+		var expected = """
+		<h3>Heading 3</h3>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingLevel4()
+	{
+		var input = "#### Heading 4";
+		var expected = """
+		<h4>Heading 4</h4>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingLevel5()
+	{
+		var input = "##### Heading 5";
+		var expected = """
+		<h5>Heading 5</h5>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingLevel6()
+	{
+		var input = "###### Heading 6";
+		var expected = """
+		<h6>Heading 6</h6>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithClosingSequence()
+	{
+		var input = "# Heading 1 #";
+		var expected = """
+		<h1>Heading 1</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithMultipleClosingHashes()
+	{
+		var input = "## Heading 2 ###";
+		var expected = """
+		<h2>Heading 2</h2>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithClosingHashesAndSpaces()
+	{
+		var input = "# Heading 1 #  ";
+		var expected = """
+		<h1>Heading 1</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithInlineEmphasis()
+	{
+		var input = "# *Heading* with **emphasis**";
+		var expected = """
+		<h1><em>Heading</em> with <strong>emphasis</strong></h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithInlineCode()
+	{
+		var input = "# Heading with `code`";
+		var expected = """
+		<h1>Heading with <code>code</code></h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithLink()
+	{
+		var input = "# Heading with [link](https://example.com)";
+		var expected = """
+		<h1>Heading with <a href="https://example.com">link</a></h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void SetextHeadingLevel1WithEquals()
+	{
+		var input = """
+		Heading 1
+		========
+		""";
+		var expected = """
+		<h1>Heading 1</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void SetextHeadingLevel2WithDashes()
+	{
+		var input = """
+		Heading 2
+		--------
+		""";
+		var expected = """
+		<h2>Heading 2</h2>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void SetextHeadingWithMultilineContent()
+	{
+		var input = """
+		Heading 1
+		line 2
+		========
+		""";
+		var expected = """
+		<h1>Heading 1
+		line 2</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void SetextHeadingWithInlineFormatting()
+	{
+		var input = """
+		*Heading* 1
+		========
+		""";
+		var expected = """
+		<h1><em>Heading</em> 1</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWith3SpaceIndent()
+	{
+		var input = "   # Heading 1";
+		var expected = """
+		<h1>Heading 1</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWith4SpaceIndentShouldBeCode()
+	{
+		var input = "    # Heading 1";
+		var expected = """
+		<pre><code># Heading 1
+		</code></pre>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithoutSpaceAfterHashIsParagraph()
+	{
+		var input = "#Not a heading";
+		var expected = """
+		<p>#Not a heading</p>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWith7HashCharactersIsParagraph()
+	{
+		var input = "####### Not a heading";
+		var expected = """
+		<p>####### Not a heading</p>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithEmptyContent()
+	{
+		var input = "# ";
+		var expected = """
+		<h1></h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithOnlyHashAndClosingHash()
+	{
+		var input = "## #";
+		var expected = """
+		<h2></h2>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void SetextHeadingRequiresParagraphContent()
+	{
+		var input = """
+		- Not a heading
+		========
+		""";
+		var expected = """
+		<ul>
+		<li>Not a heading
+		========</li>
+		</ul>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingEscapesClosingHashWithBackslash()
+	{
+		var input = "# Heading with \\# escaped";
+		var expected = """
+		<h1>Heading with # escaped</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingAtEndOfDocument()
+	{
+		var input = "# Last heading";
+		var expected = """
+		<h1>Last heading</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void MultipleAtxHeadings()
+	{
+		var input = """
+		# Heading 1
+		## Heading 2
+		### Heading 3
+		""";
+		var expected = """
+		<h1>Heading 1</h1>
+		<h2>Heading 2</h2>
+		<h3>Heading 3</h3>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void MultipleSetextHeadings()
+	{
+		var input = """
+		Heading 1
+		========
+
+		Heading 2
+		--------
+		""";
+		var expected = """
+		<h1>Heading 1</h1>
+		<h2>Heading 2</h2>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingPrecededByParagraphWithoutBlankLine()
+	{
+		var input = """
+		Paragraph
+		# Heading
+		""";
+		var expected = """
+		<p>Paragraph</p>
+		<h1>Heading</h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+
+	[TestMethod]
+	public void AtxHeadingWithMixedInlineElements()
+	{
+		var input = "# **Bold** text, *italic* text, `code`, and [link](https://example.com)";
+		var expected = """
+		<h1><strong>Bold</strong> text, <em>italic</em> text, <code>code</code>, and <a href="https://example.com">link</a></h1>
+		""";
+		var doc = Parser.Execute(input, Core.RuleSet, false);
+		var html = RenderHtml.Execute(doc, Core.RuleSet.Renderers);
+		Assert.AreEqual(expected.Trim(), html.Trim());
+	}
+}
